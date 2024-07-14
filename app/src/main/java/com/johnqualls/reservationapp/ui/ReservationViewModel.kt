a771433a2f3c9084e372f25563f958e88d7f431f
John Qualls <john.qualls@headspace.com>
2024-07-13 20:13:10 -0500

    Add empty view model

---
+++ app/src/main/java/com/johnqualls/reservationapp/ui/ReservationViewModel.kt
@@ -0,0 +1,8 @@
+package com.johnqualls.reservationapp.ui
+
+import androidx.lifecycle.ViewModel
+import com.johnqualls.reservationapp.data.ReservationDataSource
+
+class ReservationViewModel(private val reservationDataSource: ReservationDataSource) : ViewModel() {
+
+}
