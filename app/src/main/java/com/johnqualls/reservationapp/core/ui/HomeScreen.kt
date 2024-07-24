82699ba5166a6cbcd9c16b016ad16363dee15141
John Qualls <john.qualls@headspace.com>
2024-07-23 23:36:07 -0500

    Add home screen

---
+++ app/src/main/java/com/johnqualls/reservationapp/core/ui/HomeScreen.kt
@@ -0,0 +1,53 @@
+package com.johnqualls.reservationapp.core.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.johnqualls.reservationapp.R
+import com.johnqualls.reservationapp.core.ui.theme.ReservationAppTheme
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeScreenPreview() {
+    ReservationAppTheme {
+        HomeScreen()
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            modifier = Modifier
+                .size(150.dp)
+                .aspectRatio(145 / 57f),
+            painter = painterResource(id = R.drawable.henry_logo_black),
+            contentDescription = "Henry Meds Logo"
+        )
+        Button(modifier = Modifier.width(150.dp), onClick = { /*TODO*/ }) {
+            Text(text = "Provider")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(modifier = Modifier.width(150.dp), onClick = { /*TODO*/ }) {
+            Text(text = "Client")
+        }
+    }
+}
